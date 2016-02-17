509426498397830e71caff70ccc3b5cdde02e13a
Eilw <eirikwiig@gmail.com>
2016-02-17 14:19:39 +0000

    started with html version

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,21 +1,22 @@
+'use strict';
 describe("Thermostat",function(){
-  var thermo, STARTTEMP, MINTEMP, MAXPOWERSAVINGON, MAXPOWERSAVINGOFF;
+  var thermo, START_TEMP, MIN_TEMP, MAX_TEMP_POWER_SAVING_ON, MAX_TEMP_POWER_SAVING_OFF;
 
   beforeEach(function(){
     thermo = new Thermostat();
-    STARTTEMP = thermo.STARTTEMP;
-    MINTEMP = thermo.MINTEMP;
-    MAXPOWERSAVINGON = thermo.MAXPOWERSAVINGON;
-    MAXPOWERSAVINGOFF = thermo.MAXPOWERSAVINGOFF;
+    START_TEMP = thermo.START_TEMP;
+    MIN_TEMP = thermo.MIN_TEMP;
+    MAX_TEMP_POWER_SAVING_ON = thermo.MAX_TEMP_POWER_SAVING_ON;
+    MAX_TEMP_POWER_SAVING_OFF = thermo.MAX_TEMP_POWER_SAVING_OFF;
   });
 
   describe("standard setup of the thermostat",function(){
     it("the default temperature is 20",function(){
-      expect(thermo.checkTemperature()).toEqual(STARTTEMP)
+      expect(thermo.checkTemperature()).toEqual(START_TEMP);
     });
 
     it("power saving mode is on by default",function(){
-      expect(thermo.isPowerSavingOn()).toEqual(true)
+      expect(thermo.isPowerSavingOn()).toEqual(true);
     });
 
   });
@@ -23,17 +24,17 @@ describe("Thermostat",function(){
   describe("controlling the thermo",function(){
     it("Up button increases the temperature with one",function(){
       thermo.upButton();
-      expect(thermo.checkTemperature()).toEqual(STARTTEMP+1)
+      expect(thermo.checkTemperature()).toEqual(START_TEMP+1);
     });
 
     it("Down button decreases the temperature with one",function(){
       thermo.downButton();
-      expect(thermo.checkTemperature()).toEqual(STARTTEMP-1)
+      expect(thermo.checkTemperature()).toEqual(START_TEMP-1);
     });
 
     it("The minimum temperature is 10 degrees",function(){
-      for (var i = 0; i<(STARTTEMP-MINTEMP); i++){thermo.downButton();}
-      expect(thermo.checkTemperature()).toEqual(MINTEMP);
+      for (var i = 0; i<(START_TEMP-MIN_TEMP); i++){thermo.downButton();}
+      expect(thermo.checkTemperature()).toEqual(MIN_TEMP);
     });
 
   });
@@ -41,30 +42,53 @@ describe("Thermostat",function(){
   describe("Power saving mode controll",function(){
     it("Power saving mode can be turned off",function(){
       thermo.switchPowerSavingMode();
-      expect(thermo.isPowerSavingOn()).toEqual(false)
+      expect(thermo.isPowerSavingOn()).toEqual(false);
     });
 
     it("Power saving mode can be turned on",function(){
       thermo.switchPowerSavingMode();
       thermo.switchPowerSavingMode();
-      expect(thermo.isPowerSavingOn()).toEqual(true)
+      expect(thermo.isPowerSavingOn()).toEqual(true);
     });
 
   });
 
   describe("Power saving mode is ON",function(){
     it("the max temperature is 25",function(){
-      for (var i = 0; i < (MAXPOWERSAVINGON-STARTTEMP); i++){thermo.upButton();}
-      expect(thermo.checkTemperature()).toEqual(MAXPOWERSAVINGON)
+      for (var i = 0; i < (MAX_TEMP_POWER_SAVING_ON-START_TEMP); i++){thermo.upButton();}
+      expect(thermo.checkTemperature()).toEqual(MAX_TEMP_POWER_SAVING_ON);
     });
 
   });
   describe("Power saving mode is OFF",function(){
     it("the max temperature is 32",function(){
-      for (var i = 0; i < (MAXPOWERSAVINGOFF-STARTTEMP); i++){thermo.upButton();}
-      expect(thermo.checkTemperature()).toEqual(MAXPOWERSAVINGOFF)
+      thermo.switchPowerSavingMode();
+      for (var i = 0; i < (MAX_TEMP_POWER_SAVING_OFF-START_TEMP); i++){thermo.upButton();}
+      expect(thermo.checkTemperature()).toEqual(MAX_TEMP_POWER_SAVING_OFF);
     });
 
+  });
+  describe("resetTemperature",function(){
+    it("turns the thermostat back to its startemp",function(){
+      thermo.resetTemperature();
+      expect(thermo.checkTemperature()).toEqual(START_TEMP);
+    });
+  });
+
+  describe("displayColour",function(){
+    it("shows green if under 18",function(){
+      for(var i = 0; i < 3; i++){thermo.downButton();};
+      expect(thermo.displayColour()).toEqual('low-usage');
+    });
+
+    it("shows yellow if under 25",function(){
+      expect(thermo.displayColour()).toEqual('medium-usage');
+    });
+
+    it("shows red if equal to or over 25",function(){
+      for(var i = 0; i < 5; i++){thermo.upButton();};
+      expect(thermo.displayColour()).toEqual('high-usage');
+    });
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,20 +1,22 @@
 function Thermostat() {
-  this.STARTTEMP = 20;
-  this.MINTEMP = 10;
-  this.MAXPOWERSAVINGON = 25;
-  this.MAXPOWERSAVINGOFF = 32;
-  this._temperature = this.STARTTEMP;
+  this.START_TEMP = 20;
+  this.MIN_TEMP = 10;
+  this.MAX_TEMP_POWER_SAVING_ON = 25;
+  this.MAX_TEMP_POWER_SAVING_OFF = 32;
+  this.MID_ENERGY_USAGE_LIMIT = 18;
+  this.MAX_ENERGY_USAGE_LIMIT = 25;
+  this._temperature = this.START_TEMP;
   this.powerSavingMode = true;
 
 }
 
 Thermostat.prototype.upButton = function () {
-  var maxTemp = this.isPowerSavingOn() ? this.MAXPOWERSAVINGON : this.MAXPOWERSAVINGOFF;
+  var maxTemp = this.isPowerSavingOn() ? this.MAX_TEMP_POWER_SAVING_ON : this.MAX_TEMP_POWER_SAVING_OFF;
   if(this.checkTemperature() < maxTemp){this._temperature += 1;}
 };
 
 Thermostat.prototype.downButton = function () {
-  if(this.checkTemperature() > this.MINTEMP){this._temperature -= 1};
+  if(this.checkTemperature() > this.MIN_TEMP){this._temperature -= 1};
 };
 
 Thermostat.prototype.checkTemperature = function () {
@@ -27,4 +29,18 @@ Thermostat.prototype.isPowerSavingOn = function() {
 
 Thermostat.prototype.switchPowerSavingMode = function() {
   this.powerSavingMode = !this.powerSavingMode;
+}
+
+Thermostat.prototype.resetTemperature = function() {
+  this._temperature = this.START_TEMP;
+}
+
+Thermostat.prototype.displayColour = function() {
+  if(this.checkTemperature() < this.MID_ENERGY_USAGE_LIMIT){
+    return 'low-usage';
+  }else if((this.checkTemperature() < this.MAX_ENERGY_USAGE_LIMIT)){
+    return 'medium-usage';
+  }else{
+    return 'high-usage';
+  };
 }

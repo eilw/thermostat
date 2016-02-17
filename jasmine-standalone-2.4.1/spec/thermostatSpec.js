590042a3186a2da89169a65a7f382424bab3f140
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-02-17 15:28:07 +0000

    Power saving off tested and implemented.

--- jasmine-standalone-2.4.1/spec/thermostatSpec.js
+++ jasmine-standalone-2.4.1/spec/thermostatSpec.js
@@ -34,8 +34,19 @@ describe('thermostat', function(){
   });
 
   describe('PSM', function(){
-    it('PSM is on by default', function(){
+    it('sets PSM to on by default', function(){
       expect(thermostat.isPSMOn()).toEqual(true);
+    });
+
+    it('sets the maximum temperature to twenty five degrees when engaged', function(){
+      for(var i = 0; i<(thermostat.MAX_TEMP-thermostat.START_TEMP); i++){thermostat.upButton();};
+      expect(function(){thermostat.upButton()}).toThrow('Max temperature is '+ thermostat.MAX_TEMP)
+    });
+
+    it('sets the maximum temperature to thirty two degrees when disengaged', function(){
+      thermostat.switchPSM()
+      for(var i = 0; i<(thermostat.GREEDY_MAX_TEMP-thermostat.START_TEMP); i++){thermostat.upButton();};
+      expect(function(){thermostat.upButton()}).toThrow('Max temperature is '+ thermostat.GREEDY_MAX_TEMP)
     });
   });
 });
--- jasmine-standalone-2.4.1/src/thermostat.js
+++ jasmine-standalone-2.4.1/src/thermostat.js
@@ -3,9 +3,14 @@ function Thermostat(){
   this.MIN_TEMP = 10;
   this._temperature = this.START_TEMP;
   this._PSMOn = true;
+  this.MAX_TEMP = 25;
+  this.GREEDY_MAX_TEMP = 32
 };
 
 Thermostat.prototype.upButton = function(){
+  if(this.showTemperature() === this._maxTemp()) {
+    throw ('Max temperature is ' + this._maxTemp());
+  };
   this._temperature += 1;
 };
 
@@ -22,4 +27,16 @@ Thermostat.prototype.showTemperature = function(){
 
 Thermostat.prototype.isPSMOn = function(){
   return this._PSMOn;
+};
+
+Thermostat.prototype.switchPSM = function(){
+  this._PSMOn = !this._PSMOn;
+};
+
+Thermostat.prototype._maxTemp = function(){
+  if (this.isPSMOn()) {
+    return this.MAX_TEMP;
+  } else {
+    return this.GREEDY_MAX_TEMP;
+  };
 };
